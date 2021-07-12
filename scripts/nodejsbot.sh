fb081a1dbfc6a0c6521c9864e5815506587bee68
caseyCantCode <77986712+caseyCantCode@users.noreply.github.com>
2021-07-12 14:42:46 -0600

    Update nodejsbot.sh

--- scripts/nodejsbot.sh
+++ scripts/nodejsbot.sh
@@ -1,11 +1,1 @@
-echo "Installing Node.js v14 on your system"
-echo "Checking if permissions are valid"
-if [ "$EUID" -ne 0 ]
-  then echo "Please run this script as root"
-  exit
-fi
-apt update
-apt install -y curl git
-curl -sL https://deb.nodesource.com/setup_14.x | bash -
-apt -y install nodejs
-npm i -g pm2 nodemon
+lol
