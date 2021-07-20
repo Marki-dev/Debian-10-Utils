096237c5be25693642b29c499bf2e927d917836a
DanPlayz <damuffin36@gmail.com>
2021-07-19 17:04:33 -0700

    Removed keyword `sudo`

--- scripts/mongodb.sh
+++ scripts/mongodb.sh
@@ -3,8 +3,8 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 apt-get install -y gnupg
-wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
-echo "deb http://repo.mongodb.org/apt/debian buster/mongodb-org/5.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | apt-key add -
+echo "deb http://repo.mongodb.org/apt/debian buster/mongodb-org/5.0 main" | tee /etc/apt/sources.list.d/mongodb-org-5.0.list
 apt-get update
 apt-get install -y mongodb-org
 systemctl daemon-reload
