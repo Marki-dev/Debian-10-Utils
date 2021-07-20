0631b43b74ea64562dce52df00386eefcc3bdbc6
DanPlayz <damuffin36@gmail.com>
2021-07-19 17:00:42 -0700

    Create mongodb.sh

---
+++ scripts/mongodb.sh
@@ -0,0 +1,11 @@
+if [ "$EUID" -ne 0 ]
+  then echo "Please run this script as root"
+  exit
+fi
+apt-get install -y gnupg
+wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
+echo "deb http://repo.mongodb.org/apt/debian buster/mongodb-org/5.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+apt-get update
+apt-get install -y mongodb-org
+systemctl daemon-reload
+systemctl start mongod
